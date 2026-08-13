cda73b4bef60f1278049fd7e1cf1e7c99dc02cd5
Charlyken <129963596+Charlyken@users.noreply.github.com>
2026-08-13 13:28:38 +0000

    fix(coding-agent): corriger les defauts de fonctionnement
     - ajout d'une memoire au LLM
     - restriction de l'espace d'intervention de LLM dans la codeBase

--- coding-agent/src/main/java/com/charlyken/codingagent/CodingAgentApplication.java
+++ coding-agent/src/main/java/com/charlyken/codingagent/CodingAgentApplication.java
@@ -1,5 +1,6 @@
 package com.charlyken.codingagent;
 
+import java.nio.file.Path;
 import java.util.Map;
 import java.util.Scanner;
 
@@ -8,6 +9,9 @@ import org.springaicommunity.agent.tools.GlobTool;
 import org.springaicommunity.agent.tools.GrepTool;
 import org.springaicommunity.agent.tools.ShellTools;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -22,60 +26,69 @@ public class CodingAgentApplication {
 		SpringApplication.run(CodingAgentApplication.class, args);
 	}
 
-    @Bean
-	CommandLineRunner commandLineRunner (ChatClient.Builder builder) {
+	@Bean
+	CommandLineRunner commandLineRunner(ChatClient.Builder builder) {
 		return args -> {
 			String workingDirectory = System.getProperty("user.dir");
 
+			ChatMemory chatMemory = MessageWindowChatMemory.builder()
+					.maxMessages(150)
+					.build();
+
 			ChatClient client = builder
-			.defaultSystem("""
-				Tu es un assistant de developpement.
-				Tu peux inspecter le projet situé dans : %s
+					.defaultSystem("""
+								Tu es un assistant de developpement.
+								Tu peux inspecter le projet situé dans : %s
 
-				N'invente jamais le contenu d'un fichier.
-				Lis le avant de l'expliquer.
-			""".formatted(workingDirectory))
-			.defaultTools(
-				FileSystemTools.builder().build(),
-				GrepTool.builder().build(),
-				GlobTool.builder().build(),
-				ShellTools.builder().build()
+								N'invente jamais le contenu d'un fichier.
+								Lis le avant de l'expliquer.
+							""".formatted(workingDirectory)
+					)
+					.defaultTools(
+							FileSystemTools.builder()
+									.allowedDirectories(Path.of(workingDirectory))
+									.build(),
+							GrepTool.builder().build(),
+							GlobTool.builder().build(),
+							ShellTools.builder().build()
 
-			)
-			.build();
+					)
+					.defaultAdvisors(
+							MessageChatMemoryAdvisor.builder(chatMemory)
+									.build()
+					)
+					.build();
 
-			try (Scanner sc = new Scanner(System.in)) { 
-				System.out.println("Agent prêt. Ecris exit pour quitter.");
+			try (Scanner sc = new Scanner(System.in)) {
+				System.out.println("\nAgent prêt. Ecris exit pour quitter.\n");
 
 				while (true) {
 					System.out.println("> ");
 					String input = sc.nextLine();
 
-					if ("exit".equalsIgnoreCase(input)){
+					if ("exit".equalsIgnoreCase(input)) {
 						break;
 					}
 
 					try {
 						Flux<String> response = client.prompt()
-							.system("Tu es un expert du developpement logiciel")
-							.user(input)
-							.toolContext(Map.of("workingDirectory", workingDirectory))
-							.stream()
-							.content();
-					
-					   response
-							.doOnNext(System.out::print)
-							.blockLast();
-				
-			
-					System.out.println();
+								.user(input)
+								.toolContext(Map.of("workingDirectory", workingDirectory))
+								.stream()
+								.content();
+						System.out.println("\n");
+						response
+								.doOnNext(System.out::print)
+								.blockLast();
+
+						System.out.println();
 					} catch (Exception e) {
 						System.err.println("Erreur: " + e.getMessage());
 					}
 				}
 
-		    }		
-					
+			}
+
 		};
 	}
 
